f2be23d6ab704c51f5f310bbbbe986022ee6faca
sophia kim <sophkim26@bergen.org>
2025-03-03 11:33:12 -0500

    set up pubsub

---
+++ Assets/Scripts/PubSub Pattern/Message.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * We'll see if we need this class -- for now it's just a placeholder for
+ * future data that might need to get passed beyond the call itself.
+ */
+public class Message
+{
+    
+}
