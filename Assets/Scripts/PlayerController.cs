33f5ea667dc61d3ff4277a5bb1ae9e63ecfc44a7
aekmekci72 <anna.ekmekci@gmail.com>
2025-03-04 20:57:06 -0500

    Thinking about single responsibility with powerup handling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,17 +12,17 @@ public class PlayerController : MonoBehaviour
     public Transform[] firePoints;
     public float fireRate = 0.5f;
     private float nextFireTime = 0f;
-    public bool takingDamage =true;
-    private List<Powerup> activePowerups = new List<Powerup>();
+    public bool takingDamage = true;
     public float health = 1f;
 
-    // Start is called before the first frame update
+    private PowerupHandler powerupManager;
+
     void Start()
     {
         CalculateScreenBoundaries();
+        powerupManager = GetComponent<PowerupHandler>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePlayer();
@@ -33,7 +33,6 @@ public class PlayerController : MonoBehaviour
     {
         Camera mainCamera = Camera.main;
         Vector3 screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
-        
         minX = -screenBounds.x + boundaryOffset;
         maxX = screenBounds.x - boundaryOffset;
     }
@@ -41,7 +40,7 @@ public class PlayerController : MonoBehaviour
     void MovePlayer()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontalInput*moveSpeed*Time.deltaTime, 0f,0f);
+        Vector3 movement = new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f);
         Vector3 newPosition = transform.position + movement;
         newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
         transform.position = newPosition;
@@ -49,39 +48,18 @@ public class PlayerController : MonoBehaviour
 
     void CheckShoot()
     {
-    if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextFireTime)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextFireTime)
         {
             Shoot();
-            nextFireTime= Time.time + fireRate;
+            nextFireTime = Time.time + fireRate;
         }
     }
+
     void Shoot()
     {
         foreach (Transform firePoint in firePoints)
         {
             Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        }   
-    }
-    public void AddPowerup(Powerup powerup)
-    {
-        activePowerups.Add(powerup);
-        powerup.ApplyEffect(this);
-    }
-    public void RemovePowerup(Powerup powerup)
-    {
-        if (activePowerups.Contains(powerup))
-        {
-            activePowerups.Remove(powerup);
-            powerup.RemoveEffect(this);
         }
-    }
-
-    private void OnDestroy()
-    {
-        foreach (Powerup powerup in activePowerups)
-        {
-            powerup.RemoveEffect(this);
-        }
-        activePowerups.Clear();
     }
 }
--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -1,21 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Powerup : MonoBehaviour
 {
     public abstract void ApplyEffect(PlayerController player);
     public abstract void RemoveEffect(PlayerController player);
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.CompareTag("Player"))
+        PowerupHandler powerupManager = other.GetComponent<PowerupHandler>(); 
+
+        if (powerupManager != null)
         {
-            PlayerController player = collision.GetComponent<PlayerController>();
-            if (player != null)
-            {
-                player.AddPowerup(this);
-                Hide();
-            }
+            powerupManager.AddPowerup(this);
+            Hide();
         }
     }
     private void Hide()
