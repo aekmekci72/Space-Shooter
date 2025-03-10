a3bead0c6ce2d7ebd5e7520da89d9921180231e0
Sylvia Lee <sylvieelee@gmail.com>
2025-03-10 02:37:37 -0400

    update test

--- Assets/Tests/EditMode/HealthTests.cs
+++ Assets/Tests/EditMode/HealthTests.cs
@@ -5,7 +5,7 @@
 //
 // This suite also ensures that the Player loses health accordingly, when it is hit with an Enemy Bullet
 // and displays a death message if the Player were to lose all of their health.
-// I used NSubstitute to mock dependencies like a mockPlayer, poweruphandler, etc... to ensure that 
+// I used NSubstitute to mock dependencies like a mockPlayer, poweruphandler, etc... which allows me to focus soley on the Player's health
 
 using NUnit.Framework;
 using UnityEngine;
@@ -33,6 +33,13 @@ public class HealthTests
         enemyBullet = bulletObject.AddComponent<Ebullet>();
 
         mockPlayer = Substitute.For<PlayerController>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(bulletObject);
+        Object.Destroy(powerupObject);
     }
 
     [Test]
@@ -66,71 +73,35 @@ public class HealthTests
         Assert.AreEqual(10, mockPlayer.health);
     }
 
-    [TearDown]
-    public void Teardown()
+
+    [Test]
+    public void AddPowerup_ShouldAddToListAndApplyEffect()
     {
-        Object.Destroy(bulletObject);
+        // Arrange
+        var mockPowerup = Substitute.For<Powerup>();
+
+        // Act
+        powerupHandler.AddPowerup(mockPowerup);
+
+        // Assert
+        Assert.That(powerupHandler.GetActivePowerups(), Does.Contain(mockPowerup));
+        mockPowerup.Received(1).ApplyEffect(mockPlayer);
     }
 
+    [Test]
+    public void RemovePowerup_ExistingPowerup_ShouldRemoveFromListAndRemoveEffect()
+    {
+        // Arrange
+        var mockPowerup = Substitute.For<Powerup>();
+        powerupHandler.AddPowerup(mockPowerup);
 
-    // [Test]
-    // public void AddPowerup_ShouldAddToListAndApplyEffect()
-    // {
-    //     // Arrange
-    //     var mockPowerup = Substitute.For<Powerup>();
+        // Act
+        powerupHandler.RemovePowerup(mockPowerup);
 
-    //     // Act
-    //     powerupHandler.AddPowerup(mockPowerup);
+        // Assert
+        Assert.That(powerupHandler.GetActivePowerups(), Does.Not.Contain(mockPowerup));
+        mockPowerup.Received(1).RemoveEffect(mockPlayer);
+    }
 
-    //     // Assert
-    //     Assert.That(powerupHandler.GetActivePowerups(), Does.Contain(mockPowerup));
-    //     mockPowerup.Received(1).ApplyEffect(mockPlayer);
-    // }
-
-    // [Test]
-    // public void RemovePowerup_ExistingPowerup_ShouldRemoveFromListAndRemoveEffect()
-    // {
-    //     // Arrange
-    //     var mockPowerup = Substitute.For<Powerup>();
-    //     powerupHandler.AddPowerup(mockPowerup);
-
-    //     // Act
-    //     powerupHandler.RemovePowerup(mockPowerup);
-
-    //     // Assert
-    //     Assert.That(powerupHandler.GetActivePowerups(), Does.Not.Contain(mockPowerup));
-    //     mockPowerup.Received(1).RemoveEffect(mockPlayer);
-    // }
-
-    // [Test]
-    // public void RemovePowerup_NonExistingPowerup_ShouldNotRemoveEffect()
-    // {
-    //     // Arrange
-    //     var mockPowerup = Substitute.For<Powerup>();
-
-    //     // Act
-    //     powerupHandler.RemovePowerup(mockPowerup);
-
-    //     // Assert
-    //     mockPowerup.DidNotReceive().RemoveEffect(Arg.Any<PlayerController>());
-    // }
-
-    // [Test]
-    // public void OnDestroy_ShouldRemoveAllPowerupsAndClearList()
-    // {
-    //     // Arrange
-    //     var mockPowerup1 = Substitute.For<Powerup>();
-    //     var mockPowerup2 = Substitute.For<Powerup>();
-    //     powerupHandler.AddPowerup(mockPowerup1);
-    //     powerupHandler.AddPowerup(mockPowerup2);
-
-    //     // Act
-    //     powerupHandler.SendMessage("OnDestroy");
-
-    //     // Assert
-    //     mockPowerup1.Received(1).RemoveEffect(mockPlayer);
-    //     mockPowerup2.Received(1).RemoveEffect(mockPlayer);
-    //     Assert.That(powerupHandler.GetActivePowerups(), Is.Empty);
-    // }
-
+ 
 }
