d2910bff0bf03e7a4cac048817d3d553d2b160dc
Sylvia Lee <sylvieelee@gmail.com>
2025-03-12 21:59:32 -0400

    sound files

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource soundEffectSource;
+
+    [SerializeField] private float effectVolume;
+
+
+    [field: SerializeField] public AudioClip PlayerLaser { get; private set; }
+    [field: SerializeField] public AudioClip EnemyLaser { get; private set; }
+    [field: SerializeField] public AudioClip EnemyDamage { get; private set; }
+    
+
+    private AudioClip _chosenAudio;
+
+    public void Start ()
+    {
+        soundEffectSource.volume = effectVolume;
+    }
+
+
+    public void PlayEffect(AudioClip clip)
+    {
+        soundEffectSource.clip = clip;
+        soundEffectSource.Play();
+    }
+
+}
